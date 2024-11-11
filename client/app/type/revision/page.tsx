2b090b5f471f7609c8517bdc59b618ac9b9c0d9c
theUnrealSamurai <roshan.saravanan001@gmail.com>
2024-11-11 18:01:17 +0900

    Added: Page when there is nothing to revise

--- client/app/type/revision/page.tsx
+++ client/app/type/revision/page.tsx
@@ -4,6 +4,7 @@ import React, { useEffect, useState } from "react";
 import Cookies from "js-cookie";
 import Explanations from "@/components/Explanations";
 import KanjiStarCard from "@/components/KanjiStarCard";
+import Link from "next/link";
 
 interface KanjiData {
     kanji: string;
@@ -38,6 +39,9 @@ export default function Revision() {
             const data = await response.json();
 
             if (response.ok) {
+                if (data.message === "No cards to revise") {
+                    return <RevisionNotAvailable />
+                }
                 setJpSentence(data.japanese);
                 setRomaji(data.romaji);
                 setEngSentence(data.english);
@@ -148,9 +152,11 @@ export default function Revision() {
                 </div>
             </div>
 
+
             {/* Kanji Cards with stars */}
             <div className="w-[80%] ml-5 mt-5">
-                <div className="bg-[#453F3F] flex flex-col text-white box-border w-full rounded-3xl border-2 border-main-red drop-shadow-2xl shadow-white overflow-hidden">
+                <h3 className="text-2xl font-bold text-white">Kanji</h3>
+                <div className="flex flex-col text-white box-border w-full rounded-3xl">
                     <div className="flex overflow-x-auto p-4 scrollbar-thin scrollbar-thumb-main-red scrollbar-track-gray-700">
                         <div className="flex flex-nowrap">
                             {kanji.map((item, index) => (
@@ -162,13 +168,12 @@ export default function Revision() {
             </div>
             
 
-            {/* Second box container */}
-            <div className="w-[80%] ml-5 mt-5">
+            {/* Third box container */}
+            <div className="w-[80%] ml-5 mt-2">
                 {/* Title for the second box */}
-                <h2 className="text-white font-bold text-left mb-2">Upcoming Kanjis for Revision: <span className="p-1 bg-main-red rounded-xl m-1">{dueKanjis.length}</span></h2>
-
+                <h3 className="text-white font-bold text-left mb-2 text-2xl">Upcoming Kanjis for Revision: <span className="p-1 bg-main-red rounded-xl m-1">{dueKanjis.length}</span></h3>
                 {/* Upcoming Kanjis */}
-                <div className="bg-[#453F3F] flex flex-col text-white box-border w-full rounded-3xl border-2 border-main-red drop-shadow-2xl shadow-white overflow-hidden">
+                <div className="flex flex-col text-white box-border w-full rounded-3xl">
                     {/* Due Kanji list */}
                     <div className="flex overflow-x-auto p-4 scrollbar-thin scrollbar-thumb-main-red scrollbar-track-gray-700">
                         <div className="flex flex-nowrap">
@@ -177,11 +182,12 @@ export default function Revision() {
                     </div>
                 </div>
 
+
                 {/* Explanations container */}
-                <div className=" flex flex-col w-full mt-5">
+                <div className=" flex flex-col w-full mt-3">
                     {/* Title and button for the explanations */}
                     <div className="flex justify-between items-center mb-2">
-                        <h2 className="text-white font-bold text-left">Words and Explanations</h2>
+                        <h2 className="text-white font-bold text-left text-2xl">Words and Explanations</h2>
                         <button
                             onClick={handleExplanationsVisibility}
                             className="bg-main-red rounded-lg font-bold px-2 py-1 w-40 text-white"
@@ -192,7 +198,7 @@ export default function Revision() {
 
                     {/* Explanations box */}
                     {isExplanationsVisible && (
-                        <div className="bg-[#453F3F] flex flex-col text-white box-border rounded-3xl border-2 border-main-red drop-shadow-2xl shadow-white overflow-hidden">
+                        <div className="flex flex-col text-white box-border rounded-3xl ">
                             <div className="p-4 max-h-96 overflow-y-auto scrollbar-thin scrollbar-thumb-main-red scrollbar-track-gray-700">
                                 {explanations.map((item, index) => (
                                     <Explanations key={index} explanations={item} />
@@ -207,9 +213,28 @@ export default function Revision() {
     );
 }
 
+
+ function RevisionNotAvailable() {
+    return (
+        <div className="min-h-screen overflow-hidden flex flex-col bg-main-gray">
+            <div className="flex flex-grow flex-col justify-center items-center py-8">
+                <div className="-mt-24 flex flex-col items-center text-white">
+                    <h1 className="font-bold text-3xl md:text-4xl px-5 pb-2 text-center">No Cards to Revise</h1>
+                    <p className="px-7 py-2 text-center max-w-[80%]">
+                        You've completed all your revisions for now. Great job! Learn more in the Practice page or come back later to continue improving your skills.
+                    </p>
+                    <Link href="type/practice" className="py-2 px-4 m-3 font-bold text-xl md:text-2xl text-center text-white rounded-xl bg-main-red">
+                        <h2>Keep learning and practicing!</h2>
+                    </Link>
+                </div>
+            </div>
+        </div>
+    );
+}
+
 function dueKanji(kanji: string) {
     return (
-        <div key={kanji} className="bg-black w-14 h-14 flex-shrink-0 flex items-center justify-center rounded-xl border-2 border-main-red m-2">
+        <div key={kanji} className="bg-black w-14 h-14 flex-shrink-0 flex items-center justify-center rounded-xl m-2">
             <p className="font-bold text-2xl">{kanji}</p>
         </div>
     );
